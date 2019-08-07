c7d6d21cc84cfb413ef7cf848268d64975c05629
vacneyelk@gmail.com <Kyle>
2019-08-06 23:12:56 -0700

    updated to only launch bot by Vethorm class

--- launcher.py
+++ launcher.py
@@ -1,7 +1,8 @@
-# LIBRARY IMPORTS
+# STANDARD LIBRARY
 
 import asyncio
 import json
+import sys
 
 # THIRD PARTY IMPORTS
 
@@ -11,79 +12,29 @@ from discord.ext import commands
 try:
     import uvloop
 except ImportError:
-    print("uvloop was unable to import\n"
-          "regular asyncio will run instead of uvloop")
+    print('uvloop failed to import', file=sys.stderr)
 else:
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-# PROJECTS IMPORTS
+# PROJECT
 
-import utilities.secret as secret
-import utilities.vqueries as vquery
-import utilities.catalogue as catalogue
+from bot import Vethorm
 
 # CONSTANTS
 
-extensions = [
-    "cogs.courses",
-    'cogs.onhandling',
-    'cogs.tags',
-    'cogs.utility',
-    'cogs.watch'
-]
-
-prefix = ['$']
-bot = commands.Bot(command_prefix=prefix, owner_id=secret.OWNER)
-
-@bot.event
-async def on_ready() -> None:
-    print(f'Logged in as: {bot.user}')
-    print('__________________________________________')
-    game = discord.Game(name='$help for help')
-    await bot.change_presence(activity=game)
-    for guild in bot.guilds:
-        if guild.id not in bot.Vservers:
-            await vquery.insert_server(bot, guild.id)
-    print('==== Guilds Loaded ====')
 
 
+# FUNCTIONS
 
-@bot.event
-async def on_message(message):
-    # try:
-    #     await vquery.insert_server(bot.pool, message.guild.id)
-    # except Exception as e:
-    #     print(e)
-    if message.author.bot:
-        return
-    await bot.process_commands(message)
+def run_bot():
+    bot = Vethorm()
+    bot.run()
 
-async def load():
-    from pprint import pprint
-    
-    # asyncpg connection pool THIS MUST BE LOADED FIRST
-    bot.Vpool = await vquery.init_database_connection()
-    # UCICatalogueCachedScraper object for scraping
-    bot.Vcourses = catalogue.UCICatalogueCachedScraper(7*86400)
-    # dictionary course aliases
-    bot.Valiases = await vquery.request_catalogue_aliases(bot)
-    # dictionary of servers
-    bot.Vservers = await vquery.request_servers(bot)
-    # diction of users by guild id
-    bot.Vusers = await vquery.request_users(bot)
-    # dictionary of tags by server
-    bot.Vtags = await vquery.request_tags(bot)
+def main():
+    run_bot()
 
+# MAIN
 
-if __name__ == "__main__":
-    from pathlib import Path
-    print(Path.cwd())
-    for extension in extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print("Failed to load extension {exten}\n"
-                  "ERROR - {error_name}: {error}\n".format(exten=extension, error_name=type(e).__name__, error=e))
-    asyncio.get_event_loop().run_until_complete(load())
-    bot.run(secret.BOT_TOKEN)
+if __name__ == '__main__':
+    main()
 
